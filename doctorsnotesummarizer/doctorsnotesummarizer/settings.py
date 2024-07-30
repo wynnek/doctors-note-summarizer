0b9caa429ac94df98537e7e73feb241d5ee749c2
Kim Wynne <kimberly.t.wynne@gmail.com>
2024-07-30 11:15:57 -0400

    Procfile, settings.py, requirements.txt

--- doctorsnotesummarizer/doctorsnotesummarizer/settings.py
+++ doctorsnotesummarizer/doctorsnotesummarizer/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for doctorsnotesummarizer project.
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c5$$@$go@$w(yf1n0!m97%i_(34#=42*+1mf9bv822761j2m-&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['doctors-note-summarizer-9db6a1c62b84.herokuapp.com', '127.0.0.1'] # ['*']
 
 
 # Application definition
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static" # os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # BASE_DIR / "static"
 
 # MEDIA_URL = "media/"
 # MEDIA_ROOT = BASE_DIR / "media" # os.path.join(BASE_DIR, "media")
