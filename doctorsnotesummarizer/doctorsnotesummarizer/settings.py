3bb4d443b46dedf54ccc3d64a2b74512aad18358
Kim Wynne <kimberly.t.wynne@gmail.com>
2024-07-30 14:08:55 -0400

    settings.py static files

--- doctorsnotesummarizer/doctorsnotesummarizer/settings.py
+++ doctorsnotesummarizer/doctorsnotesummarizer/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA_URL = "media/"
 # MEDIA_ROOT = BASE_DIR / "media" # os.path.join(BASE_DIR, "media")
